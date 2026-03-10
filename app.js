dc9a5d23c9fec82f11d3591b51ac95a5f80f1d47
Kiruba Jegatheesan <kirubaj@hotmail.com>
2026-03-10 14:52:04 +0000

    updated cors functionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require ('express')
+const cors = require('cors')
 const fruitsApp = express()
 const fruitRoutes = require('./routes/fruitRoutes')
 
 
+fruitsApp.use(cors())
 fruitsApp.use(express.json()) // for any http method
 fruitsApp.use('/fruits',fruitRoutes)
 
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const port = process.env.PORT;
 const app = require('./app')
 
-
-
 app.listen(port, () => {
     console.log(`Fruits API is listening on port ${port}`);
 
